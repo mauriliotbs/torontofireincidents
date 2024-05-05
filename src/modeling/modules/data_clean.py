2dfc24849cc3ddd8b211461bf4649bbdc3ee84ed
ryanliy <liyanage.ryan@Gmail.com>
2024-05-04 20:20:55 -0400

    Established breakdown of pipeline modules

---
+++ src/modeling/modules/data_clean.py
@@ -0,0 +1,6 @@
+class DataCleaner:
+    def createPipeline(self):
+        return 'data_cleaner_pipeline'
+    
+    def cleanse_dataframe(df):
+        return 'cleansed_df'
---
+++ src/modeling/modules/data_reduction.py
@@ -0,0 +1,6 @@
+class FeatureAnalysis:
+    def keepStrongestFeaturesInDataFrame(self, response_variable, df):
+        '''
+        response_variable: Will measure correlation of features in relationship to the response_variable 
+        '''
+        return 'df with weak features eliminated'
