f9a57ed7c9f9a973d08c8bde8b4fb4695b698a4c
ryanliy <liyanage.ryan@Gmail.com>
2024-05-06 04:01:28 -0400

    implemented modelling without pipelines

--- src/modeling/modules/feature_transformers.py
+++ src/modeling/modules/feature_transformers.py
@@ -7,6 +7,8 @@ from sklearn.preprocessing import StandardScaler, FunctionTransformer, PowerTran
 
 class FeatureTransformer:
 
+    NUMERICAL_COLS = []
+
     CATEGORICAL_COLS = ['Area_of_Origin', 'Building_Status', 'Business_Impact', 'Extent_Of_Fire', 'Final_Incident_Type', 
                     'Fire_Alarm_System_Impact_on_Evacuation', 'Fire_Alarm_System_Operation', 'Fire_Alarm_System_Presence', 
                     'Ignition_Source', 'Initial_CAD_Event_Type', 'Material_First_Ignited', 'Method_Of_Fire_Control', 
@@ -24,11 +26,12 @@ class FeatureTransformer:
     def createTransformerPipeline(df):
         # Define categorical and numerical features
 
-        # Get column indexes to fit into transformer
-        c1_idx = [df.columns.get_loc(item) for item in FeatureTransformer.ORDINAL_COLS]
+        # Get column indexes to fit into ordinal encoder
+        #c1_idx = [df.columns.get_loc(item) for item in FeatureTransformer.ORDINAL_COLS]
+        c1_idx = [df.columns.get_loc(item) for item in FeatureTransformer.CATEGORICAL_COLS]
 
         c2_idx = [df.columns.get_loc(item) for item in FeatureTransformer.ONEHOT_COLS]
-        
+
         response_var_idx = [df.columns.get_loc(item) for item in ['Estimated_Dollar_Loss']]
 
         # Todo - Numerical features
@@ -40,13 +43,6 @@ class FeatureTransformer:
         #     ('scaler', StandardScaler())
         # ])
 
-        ordinal_categorical_transformer = Pipeline(steps=[
-            ('ordinal', OrdinalEncoder())
-        ])
-
-        onehot_categorical_transformer = Pipeline(steps=[
-            ('onehot', OneHotEncoder(handle_unknown='ignore'))
-        ])
 
                 # Custom transformer for log transformation of the response variable
         log_transformer = FunctionTransformer(func=np.log1p, validate=False)
@@ -55,10 +51,12 @@ class FeatureTransformer:
         # Apply transformers to features using ColumnTransformer
         feature_transformer = ColumnTransformer(
             transformers=[
-                ('ordinal_encode', ordinal_categorical_transformer, c1_idx),
-                ('onehot_encode', onehot_categorical_transformer, c2_idx),
-                ('log_transform_response_var', log_transformer, response_var_idx)  # Apply log transformation to response variable
+                ('ordinal_encode', OrdinalEncoder(handle_unknown="use_encoded_value", unknown_value=np.nan), c1_idx)
+                #('onehot_encode', OneHotEncoder(sparse_output=False, handle_unknown='ignore'), c2_idx)
+                #('log_transform_response_var', log_transformer, response_var_idx)  # Apply log transformation to response variable
                 #('numerical_scaler', numerical_transformer, n_idx), -- todo
-            ])
+            ], remainder='passthrough'  # Keep the remaining columns as they are
+            )
+            
 
         return feature_transformer
