155807ec1e1b70c7f903b5a417732d1ea46d090e
ryanliy <liyanage.ryan@Gmail.com>
2024-05-06 23:45:56 -0400

    enabled stronger data sanitization an excluded col

--- src/modeling/modules/data_clean.py
+++ src/modeling/modules/data_clean.py
@@ -7,24 +7,24 @@ from sklearn.impute import KNNImputer
 
 class DataCleaner:
 
-    CATEGORICAL_COLUMNS = ['Area_of_Origin','Building_Status','Business_Impact','Extent_Of_Fire','Final_Incident_Type',
+    ALL_CATEGORICAL_COLUMNS = ['Area_of_Origin','Building_Status','Business_Impact','Extent_Of_Fire','Final_Incident_Type',
 'Fire_Alarm_System_Impact_on_Evacuation','Fire_Alarm_System_Operation','Fire_Alarm_System_Presence',
 'Ignition_Source','Material_First_Ignited','Method_Of_Fire_Control','Possible_Cause',
 'Property_Use','Smoke_Alarm_at_Fire_Origin','Smoke_Alarm_at_Fire_Origin_Alarm_Failure','Smoke_Alarm_at_Fire_Origin_Alarm_Type',
 'Smoke_Alarm_Impact_on_Persons_Evacuating_Impact_on_Evacuation','Smoke_Spread','Sprinkler_System_Operation','Sprinkler_System_Presence',
 'Status_of_Fire_On_Arrival','Exposures', 'Incident_Ward']
         
-    COLUMNS_TO_KEEP = ['Area_of_Origin', 'Building_Status', 'Business_Impact', 'Extent_Of_Fire', 'Final_Incident_Type', 
+    CATEGORICAL_COLUMNS_KEPT = ['Area_of_Origin', 'Building_Status', 'Business_Impact', 'Extent_Of_Fire', 'Final_Incident_Type', 
                     'Fire_Alarm_System_Impact_on_Evacuation', 'Fire_Alarm_System_Operation', 'Fire_Alarm_System_Presence', 
                     'Ignition_Source', 'Initial_CAD_Event_Type', 'Material_First_Ignited', 'Method_Of_Fire_Control', 
                     'Possible_Cause', 'Property_Use', 'Smoke_Alarm_at_Fire_Origin', 'Smoke_Alarm_at_Fire_Origin_Alarm_Failure', 
                     'Smoke_Alarm_at_Fire_Origin_Alarm_Type', 'Smoke_Alarm_Impact_on_Persons_Evacuating_Impact_on_Evacuation', 
                     'Smoke_Spread', 'Sprinkler_System_Operation', 'Sprinkler_System_Presence', 'Status_of_Fire_On_Arrival', 
-                    'Control_Time', 'Response_Time']
+                    ]
 
 
     # List of columns to remove
-    COLUMNS_TO_REMOVE = [
+    CATEGORICAL_COLUMNS_REMOVED = [
         '_id','Exposures','Ext_agent_app_or_defer_time', 'Fire_Under_Control_Time', 'Incident_Number', 
         'Incident_Station_Area', 'Intersection', 'Last_TFS_Unit_Clear_Time', 
         'Latitude', 'Longitude', 'TFS_Alarm_Time', 'TFS_Arrival_Time','Level_Of_Origin'
@@ -36,7 +36,7 @@ class DataCleaner:
             ('imputer0', KNNImputer())
         ])
         # Get the categorical columns by index numbers
-        c_idx = [df.columns.get_loc(item) for item in DataCleaner.COLUMNS_TO_KEEP]
+        c_idx = [df.columns.get_loc(item) for item in DataCleaner.CATEGORICAL_COLUMNS_KEPT]
 
         missing_value_imputer = ColumnTransformer(transformers=[
                                                         ('imputer', categorical_column_imputer, c_idx)
@@ -58,7 +58,13 @@ class DataCleaner:
         pd.set_option('display.max_columns', None)
 
         # Remove columns from the DataFrame
-        df.drop(columns=DataCleaner.COLUMNS_TO_REMOVE, inplace=True)
+        df.drop(columns=DataCleaner.CATEGORICAL_COLUMNS_REMOVED, inplace=True)
+
+        # Remove non-numeric characters -- To sanitize discrepancies and use City of Toronto number codes
+        # Initial_CAD_Event_Type is excluded from this list since it has no number codes
+        df_cleaned = df[:]
+        for col in list(set(DataCleaner.CATEGORICAL_COLUMNS_KEPT) - set(['Initial_CAD_Event_Type'])):
+            df_cleaned[col] = df_cleaned[col].replace('[^0-9]', '', regex=True)
 
         # Replace empty strings with pd.NA (NaN)
         df_cleaned = df.mask(df == '', pd.NA)
@@ -71,7 +77,7 @@ class DataCleaner:
         df_cleaned = df_cleaned[df_cleaned['Final_Incident_Type'] != "03 - NO LOSS OUTDOOR fire (exc: Sus.arson,vandal,child playing,recycling or dump fires)"]
 
         # Converting categorical columns to string to ensure data integrity, ease the imputation process, and making the dataset ready for subsequent analysis
-        for col in DataCleaner.COLUMNS_TO_KEEP:
+        for col in DataCleaner.CATEGORICAL_COLUMNS_KEPT:
             df_cleaned.loc[:, col] = df_cleaned[col].astype('object')  # Cast to object dtype first
             df_cleaned.loc[:, col] = df_cleaned[col].astype('string')  # Then convert to string
 
